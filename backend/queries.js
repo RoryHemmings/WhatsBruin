8462174c96fcc8e6f2c88ce5ad5d09d38fb8d220
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2022-05-05 14:02:48 -0700

    add .env file to connect to remote postgres database

--- backend/queries.js
+++ backend/queries.js
@@ -1,19 +1,24 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
+require('dotenv').config();
+console.log(process.env);
+
 const pool = new Pool({
-  user: 'FAKE',
-  host: 'localhost',
-  database: 'FAKE',
-  password: 'FAKE',
-  port: 5432,
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+/*FETCHING DATA*/
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
-    response.status(200).json(results.rows)
+    response.status(200).json(results.rows);
   })
-}
+};
 module.exports = {
   getUsers,
-}
+};
