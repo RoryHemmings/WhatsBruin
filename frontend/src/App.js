f95b8469f81a1229a94cdee69f9a49b99f254d79
Caolinn Hukill <caolinn.hukill@gmail.com>
2022-05-15 21:32:34 -0700

    Add Calendar and edit Styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import './components/calendar/Calendar.css';
 import React from 'react';
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import {Route, Routes} from "react-router-dom"
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home'
 import Calendar from './components/calendar/Calendar'
 import Search from './components/search/Search';
 import Profile from './components/profile/Profile';
+
+
 
 function App() {
   return (
