ef9ba9d684c3a7cf9b561631a008e5991d74e14b
RoryHemmings <roryhemmings@ucla.edu>
2022-05-24 15:44:32 -0700

    refactor event query code

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,142 +3,105 @@ const router = express.Router();
 const db = require('../queries');
 const utils = require('../utils');
 
+function compareTime(a, b) {
+  if (a.date == b.date) {
+    if (a.time > b.time)
+      return 1;
+    else if (a.time < b.time)
+      return -1;
+    else
+      return 0;
+  }
+  if (a.date < b.date)
+    return -1;
+  else
+    return 1;
+}
+
 function getEvent(eventid) {
   return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM events WHERE id=$1', [eventid], (err, data) => {
-        if (err)
-          reject(err);
+    db.query('SELECT * FROM events WHERE id=$1', [eventid], (err, data) => {
+      if (err)
+        reject(err);
 
-        const event = data.rows[0];
-        resolve(event);
-      });
+      const event = data.rows[0];
+      resolve(event);
+    });
   });
 }
 
-const getUser = async (req, res) => {
-  const id = req.query.userid;
-  if (id == null)
-    return res.status(400).json({ message: 'no user id provided' });
-
-  db.query('SELECT * FROM users WHERE id=$1', [id], async (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ message: 'database error' });
-    }
-
-    if (data.rowCount < 1)
-      return res.status(400).json({message: 'no user with that id exists'});
-
-    let events = [];
-    const eventids = data.rows[0].addedevents.concat(data.rows[0].createdevents);
-    try {
-      for (let i = 0; i < eventids.length; i++){
-        events.push(await getEvent(eventids[i]));
-      }
-      events.sort((a,b) => {
-        if(a.date == b.date){
-          if(a.time > b.time){
-            return 1;
-          }
-          if(a.time < b.time){
-            return -1;
-          }
-          return 0;
-        }  
-        if(a.date < b.date)
-              return -1;
-        if(a.date > b.date)
-            return 1;
-      });
-      res.status(200).json({
-        events: events,
-      });
-    }
-    catch (err) {
-      return res.status(500).json({message: 'invalid event id'});
-    }
-  });
-};
-const getUserAddedEvents = async (req, res) => {
-  const id = req.query.userid;
-  if (id == null)
-    return res.status(400).json({ message: 'no user id provided' });
-
-  db.query('SELECT * FROM users WHERE id=$1', [id], async (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ message: 'database error' });
-    }
-
-    if (data.rowCount < 1)
-      return res.status(400).json({message: 'no user with that id exists'});
-
-    let events = [];
-    const eventids = data.rows[0].addedevents;
-    try {
-      for (let i = 0; i < eventids.length; i++){
-        events.push(await getEvent(eventids[i]));
-      }
-      events.sort((a,b) => {
-        if(a.date == b.date){
-          if(a.time > b.time){
-            return 1;
-          }
-          if(a.time < b.time){
-            return -1;
-          }
-          return 0;
-        }  
-        if(a.date < b.date)
-              return -1;
-        if(a.date > b.date)
-            return 1;
-      });
-      res.status(200).json({
-        events: events,
-      });
-    }
-    catch (err) {
-      return res.status(500).json({message: 'invalid event id'});
-    }
-  });
-}
-  const getUserCreatedEvents = async (req, res) => {
-    const id = req.query.userid;
+function queryEvents(id) {
+  return new Promise((resolve, reject) => {
     if (id == null)
-      return res.status(400).json({ message: 'no user id provided' });
-  
-    db.query('SELECT * FROM events WHERE organizer=$1', [id], async (err, data) => {
+      return reject({ status: 400, message: 'no user id provided' });
+
+    db.query('SELECT * FROM users WHERE id=$1', [id], async (err, data) => {
       if (err) {
         console.log(err);
-        return res.status(500).json({ message: 'database error' });
+        return reject({ status: 500, message: 'database error' });
       }
-  
+
       if (data.rowCount < 1)
-        return res.status(400).json({message: 'no user with that id exists'});
-  
-      let events = data.rows;
-      console.log(events);
-        events.sort((a,b) => {
-          if(a.date == b.date){
-            if(a.time > b.time){
-              return 1;
-            }
-            if(a.time < b.time){
-              return -1;
-            }
-            return 0;
-          }  
-          if(a.date < b.date)
-                return -1;
-          if(a.date > b.date)
-              return 1;
+        return reject({ status: 400, message: 'no user with that id exists' });
+
+      let createdEvents = [];
+      let addedEvents = [];
+      const addedEventIds = data.rows[0].addedevents;
+      const createdEventIds = data.rows[0].createdevents;
+      try {
+        for (let i = 0; i < addedEventIds.length; i++)
+          addedEvents.push(await getEvent(addedEventIds[i]));
+
+        for (let i = 0; i < createdEventIds.length; i++)
+          createdEvents.push(await getEvent(createdEventIds[i]));
+
+        addedEvents.sort(compareTime);
+        createdEvents.sort(compareTime);
+
+        return resolve({
+          addedEvents: addedEvents,
+          createdEvents: createdEvents,
         });
-        res.status(200).json({
-          events: events,
-        });
+      }
+      catch (err) {
+        return reject({ status: 500, message: 'invalid event id' });
+      }
     });
+  });
+}
+
+const getUser = async (req, res, next) => {
+  try {
+    let events = await queryEvents(req.query.userid);
+    res.status(200).json({
+      events: events.addedEvents.concat(events.createdEvents)
+    });
+  } catch (err) {
+    next(err);
   }
+};
+
+const getUserAddedEvents = async (req, res) => {
+  try {
+    let events = await queryEvents(req.query.userid);
+    res.status(200).json({
+      events: events.addedEvents
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+const getUserCreatedEvents = async (req, res) => {
+  try {
+    let events = await queryEvents(req.query.userid);
+    res.status(200).json({
+      events: events.createdEvents
+    });
+  } catch (err) {
+    next(err);
+  } 
+}
 
 const getUserTags = async (request, response) => {
   let userid = request.query.userid;
@@ -147,13 +110,13 @@ const getUserTags = async (request, response) => {
       console.log(error);
       return results.status(500).json({ message: 'database error' });
     }
-    if (results.rowCount < 1){
-      return response.status(400).json({message: 'no user with that id exists'});
+    if (results.rowCount < 1) {
+      return response.status(400).json({ message: 'no user with that id exists' });
     }
-  response.status(200).json({likes: results.rows});
-
+    response.status(200).json({ likes: results.rows });
   });
 }
+
 const postAddEvent = async (request, response) => {
   let eventid = request.body.eventid;
   let userid = request.body.userid;
@@ -161,13 +124,13 @@ const postAddEvent = async (request, response) => {
     [eventid, userid, eventid], (error, results) => {
       if (error) {
         console.log(error);
-        return response.status(500).send({message: 'database error'});
+        return response.status(500).send({ message: 'database error' });
       }
     });
   db.query('UPDATE events SET num_attendee = num_attendee + 1 WHERE id=$1', [eventid], (error, results) => {
     if (error) {
       console.log(error);
-      return response.status(500).send({message: 'database error'});
+      return response.status(500).send({ message: 'database error' });
     }
   });
   response.status(201).send({ add: eventid });
@@ -181,17 +144,16 @@ const postRemoveEvent = async (request, response) => {
     [eventid, userid, eventid], (error, results) => {
       if (error) {
         console.log(error);
-        return response.status(500).send({message: 'database error'});
+        return response.status(500).send({ message: 'database error' });
       }
     });
   db.query('UPDATE events SET num_attendee = num_attendee - 1 WHERE id=$1', [eventid], (error, results) => {
     if (error) {
       console.log(error);
-      return response.status(500).send({message: 'database error'});
+      return response.status(500).send({ message: 'database error' });
     }
   });
   response.status(201).send({ remove: eventid });
-
 };
 
 const postAddLike = async (request, response) => {
@@ -204,7 +166,6 @@ const postAddLike = async (request, response) => {
       }
     });
   response.status(201).send({ add: tagid });
-
 };
 
 const postRemoveLike = async (request, response) => {
@@ -220,7 +181,7 @@ const postRemoveLike = async (request, response) => {
 };
 
 router.get('/', utils.authenticateToken, getUser);
-router.get('/addedevents',  getUserAddedEvents)
+router.get('/addedevents', getUserAddedEvents)
 router.get('/createdevents', getUserCreatedEvents);
 router.get('/tags', getUserTags);
 router.post('/addevent', postAddEvent);
