ad3fc868b4c60a4651cc3f15834a2078e25136b0
RoryHemmings <roryhemmings@ucla.edu>
2022-05-24 16:24:27 -0700

    add base email code and fix search bug

--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,32 @@
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer')
+
+const EMAIL = process.env.EMAIL;
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  }
+});
+
+function sendEmail(recipient, subject, content) {
+  const options = {
+    from: EMAIL,
+    to: recipient,
+    subject: subject,
+    text: content,
+  }
+
+  transporter.sendMail(options, (err, info) => {
+    if (error) {
+      console.log("Email failed to send: ", err)
+    } else {
+      console.log(`Email sent to: ${options.recipient}` + info.response);
+    }
+  });
+}
 
 /* Authentication Middleware */
 function authenticateToken(req, res, next) {
