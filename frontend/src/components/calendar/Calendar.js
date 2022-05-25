678230b30c6c8c8136c024beb6784b3a1ebbda23
Caolinn Hukill <caolinn.hukill@gmail.com>
2022-05-25 12:42:26 -0700

    Deleted Unnecessary Lines

--- frontend/src/components/calendar/Calendar.js
+++ frontend/src/components/calendar/Calendar.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './Calendar.css';
 //import FullCalendar from '@fullcalendar/react'; // must go before plugins
-import FullCalendar, { formatDate } from '@fullcalendar/react'
+import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
@@ -27,8 +27,6 @@ export default class Calendar extends React.Component {
   }
   }
 
-  
-
   componentDidMount() {
     fetch("http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/home?date="+ todayStr, {
       headers: {
@@ -45,8 +43,6 @@ export default class Calendar extends React.Component {
       });
  
   }
-
-
 
 
   render() {
@@ -99,9 +95,7 @@ export default class Calendar extends React.Component {
         property = {{
           contentHeight: 550,
         }}
-        // events={
-        // //  url: "http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/home?date=2022-05-22",
-        // }
+
         weekends={this.state.weekendsVisible}
         
         events={eventList} // alternatively, use the `events` setting to fetch from a feed
@@ -119,69 +113,12 @@ export default class Calendar extends React.Component {
 
   );
       }
-//}
 
-
-renderSidebar() {
-  return (
-    <div className='demo-app-sidebar'>
-      <div className='demo-app-sidebar-section'>
-        <h2>Instructions</h2>
-        <ul>
-          <li>Select dates and you will be prompted to create a new event</li>
-          <li>Drag, drop, and resize events</li>
-          <li>Click an event to delete it</li>
-        </ul>
-      </div>
-      <div className='demo-app-sidebar-section'>
-        <label>
-          <input
-            type='checkbox'
-            checked={this.state.weekendsVisible}
-            onChange={this.handleWeekendsToggle}
-          ></input>
-          toggle weekends
-        </label>
-      </div>
-      <div className='demo-app-sidebar-section'>
-        <h2>All Events ({this.state.currentEvents.length})</h2>
-        <ul>
-          {this.state.currentEvents.map(renderSidebarEvent)}
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-handleWeekendsToggle = () => {
-  this.setState({
-    weekendsVisible: !this.state.weekendsVisible
-  })
-}
-
-handleDateSelect = (selectInfo) => {
-  let title = prompt('Please enter a new title for your event')
-  let calendarApi = selectInfo.view.calendar
-
-  calendarApi.unselect() // clear date selection
-
-  if (title) {
-    calendarApi.addEvent({
-      id: createEventId(),
-      title,
-      start: selectInfo.startStr,
-      end: selectInfo.endStr,
-      allDay: selectInfo.allDay
-    })
-  }
-}
 
 handleEventClick = (clickInfo) => {
-  //console.log(clickInfo.event.extendedProps.location);
+
   popup(clickInfo.event, {type: "info", timeout: 1000});
-  // if (prompt(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-  //   clickInfo.event.remove()
-  // }
+
 }
 
 handleEvents = (events) => {
@@ -201,14 +138,7 @@ return (
 )
 }
 
-function renderSidebarEvent(event) {
-return (
-  <li key={event.id}>
-    <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
-    <i>{event.title}</i>
-  </li>
-)
-}
+
 
 
 //popup
@@ -220,7 +150,6 @@ const popup = (Event, {type, timeout}) => {
   var description = Event.extendedProps.description;
   var category = Event.extendedProps.category;
   var location = Event.extendedProps.location;
-  //console.log(description);
   const PopupContent = () => {
     return (
 
@@ -242,10 +171,7 @@ const popup = (Event, {type, timeout}) => {
       {description}
       <p>
       <br />
-      {/* <br />
-      <br />
-      <br />
-      <br /> */}
+     
       <span style = {{fontWeight: 'bold' }}>Category:</span> {"\t"} 
       {category}
       <br />
