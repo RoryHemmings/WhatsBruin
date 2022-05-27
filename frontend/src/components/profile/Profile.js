cdf7e3ca72b335b7b3ae57b5bd2664f75b4d0626
ssalandary <86086118+ssalandary@users.noreply.github.com>
2022-05-26 23:02:40 -0700

    Implement adding and removing events through search page. Fix minor bugs in Profile page rerendering.

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -3,20 +3,25 @@ import "./Profile.css";
 import sampleImage from "./logo192.png";
 import { getWithExpiry } from "../../Token";
 import jwt_decode from "jwt-decode";
-import { Navigate } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 
 
 const Profile = () => {
 const [events, setEvents] = useState([]);
+const [addedEvents, setAddedEvents] = useState([]);
 const userInfo = jwt_decode(getWithExpiry("user"));
+const [rerender, setRerender] = useState(false);
 useEffect (() => {
+  console.log("state called");
   let user = getWithExpiry("user");
   let userInfo = jwt_decode(user);
   if(userInfo === null){
     return <Navigate to='/Signup'/>
   }
   const getData = async () => {
-    let res = await fetch ("http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/user?userid=" + userInfo.userid, {
+    let res = await fetch ("http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/user/createdevents?userid=" + userInfo.userid, {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
@@ -36,8 +41,64 @@ useEffect (() => {
   }
   getData().then(events => {
     console.log(events); setEvents(events)})
-}, [])
+  const getAddedData = async () => {
+    let res = await fetch ("http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/user/addedevents?userid=" + userInfo.userid, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + user,
+      },
+        method:"GET",
+      })
+      const status = res.status;
+      res = await res.json();
+      if(status < 400) {
+        return res.events;
+      }
+      else {
+        console.log("error");
+        return [];
+      }
+  }
+  getAddedData().then(addedEvents => {
+    console.log(addedEvents); setAddedEvents(addedEvents)})
+}, [rerender])
 
+
+const handleSubmit = async (eventid) => {
+  console.log("begin");
+  console.log("event.id: " + eventid);
+  console.log("userInfo.id: " + userInfo.userid);
+
+  let res = await fetch(
+    "http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/user/removeevent",
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        eventid: eventid,
+        userid: userInfo.userid,
+      }),
+    }
+  );
+
+  const status = res.status;
+  res = await res.json();
+
+  if (status < 400) {
+    console.log("works");
+    console.log(res);
+  } else {
+    alert(res.message);
+    alert("error stuff");
+  }
+
+  setRerender(!rerender);
+  console.log(rerender);
+};
 
   return (
     <div className="App">
@@ -52,7 +113,8 @@ useEffect (() => {
         </div>
       </div>
       <div className="profile-events">
-        <h1 className="orange">My Upcoming Events</h1>
+        <Link to="/Manager" style={{ textDecoration: 'none' }}><h1 className="orange">My Created Events</h1></Link>
+        {events?.map((event) => { }).length === 0 && "You have not created any events."}
         <div className="cards">
           {events?.map(event => {      
             return (
@@ -67,6 +129,46 @@ useEffect (() => {
             );
           })}
           </div>
+            <h1 className="orange">My Added Events</h1>
+            {addedEvents?.map((event) => { }).length === 0 && "You have not added any events."}
+        <div className="cards">
+          {addedEvents?.map((event) => {
+            return (
+              <Grid
+                className="card"
+                key={event.id}
+                noValidate
+              >
+                <div>
+                  <h2>{event.title}</h2>
+                  <h3>{event.date}</h3>
+                  <h3>
+                    {event.starttime} - {event.endtime}
+                  </h3>
+                  <h3>{event.description}</h3>
+                </div>
+                <div>
+                  <Grid container justifyContent="center">
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      onClick={() => (handleSubmit(event.id))}
+                      sx={{
+                        marginTop: 4,
+                        marginLeft: 4,
+                        bgcolor: "#ffbbbb",
+                        color: "#022A68",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      remove
+                    </Button>
+                  </Grid>
+                </div>
+              </Grid>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -4,71 +4,189 @@ import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import '../profile/Profile.css'
+import { getWithExpiry } from "../../Token";
+import jwtDecode from "jwt-decode";
 
 export default function Search() {
   const [value, setValue] = useState("");
   const [events, setEvents] = useState([]);
-  const changeHandler = e => {
-    setValue(e.target.value);
-  };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if(value === ""){
+  const [userEvents, setUserEvents] = useState([]);
+  let userInfo;
+  const user = getWithExpiry("user")
+  user ? userInfo = jwtDecode(user) : userInfo = null
+
+  const handleSearch = async (value) => {
+    if (value === "") {
       setEvents([]);
       return;
     }
-    let res = await fetch ("http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/search?title=" + value, {
+    let res = await fetch("http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/search?title=" + value, {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      method:"GET",
+      method: "GET",
     })
     const status = res.status
     res = await res.json();
-    if(status === 200){
+    if (status === 200) {
       console.log("works");
       console.log(res);
       setEvents(res);
     }
-    else{
+    else {
       alert("error!");
     }
+    if (userInfo !== null) {
+      let res2 = await fetch("http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/user/addedevents?userid=" + userInfo.userid, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + user,
+        },
+        method: "GET",
+      })
+      const status = res2.status;
+      res2 = await res2.json();
+      if (status < 400) {
+        setUserEvents(res2.events);
+      }
+      else {
+        console.log("error");
+        setUserEvents([]);
+      }
+    }
   }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (value === "") {
+      setEvents([]);
+      return;
+    }
+    handleSearch(value);
+  };
+
+  const handleDelete = async (eventid) =>{
+    console.log("begin");
+  console.log("event.id: " + eventid);
+  console.log("userInfo.id: " + userInfo.userid);
+
+  let res = await fetch(
+    "http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/user/removeevent",
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        eventid: eventid,
+        userid: userInfo.userid,
+      }),
+    }
+  );
+
+  const status = res.status;
+  res = await res.json();
+
+  if (status < 400) {
+    console.log("works");
+    console.log(res);
+  } else {
+    alert(res.message);
+    alert("error stuff");
+  }
+  handleSearch(value);
+};
+
+
+  const handleAdd = async (eventid) => {
+    console.log("begin");
+    console.log("event.id: " + eventid);
+    console.log("userInfo.id: " + userInfo.userid);
+  
+    let res = await fetch(
+      "http://ec2-50-18-101-113.us-west-1.compute.amazonaws.com:3000/user/addevent",
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          eventid: eventid,
+          userid: userInfo.userid,
+        }),
+      }
+    );
+  
+    const status = res.status;
+    res = await res.json();
+  
+    if (status < 400) {
+      console.log("works");
+      console.log(res);
+    } else {
+      alert(res.message);
+      alert("error stuff");
+    }
+    handleSearch(value);
+  };
+
+  const FindEvent = (key) => {
+    if(userEvents.some(event=>event.id===key.event)){
+      return <button onClick={function(){handleDelete(key.event)}}>remove</button>
+    }
+    return <button onClick={function(){handleAdd(key.event)}}>add</button>;
+  };
+
+  const changeHandler = e => {
+    setValue(e.target.value);
+  };
+
+
   return (
     <div className='App'>
       <Paper
-      component="form"
-      sx={{ p: '2px 4px', display: 'inline-flex', justifyContent: 'center', alignItems: 'center', width: 300, marginTop: 3 }}
-    >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search for an event"
-        inputProps={{ 'aria-label': 'Search for an event' }}
-        id = "value"
-        name = "value"
-        value = {value ?? ""}
-        onChange={changeHandler}
-      />
-      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search" onClick={handleSubmit}>
-        <SearchIcon />
-      </IconButton>
-    </Paper>
-    <div className="cards">
-          {events?.map(event => {      
-            return (
-              <div className="card" key={event.id}>
-                <div>
-                  <h2>{event.title}</h2>
-                  <h3>{event.organizeruser}</h3>
-                  <h3>{event.date}</h3>
-                  <h3>{event.starttime} - {event.endtime}</h3>
-                  <h3>{event.description}</h3>
-                </div>
+        component="form"
+        sx={{ p: '2px 4px', display: 'inline-flex', justifyContent: 'center', alignItems: 'center', width: 300, marginTop: 3 }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search for an event"
+          inputProps={{ 'aria-label': 'Search for an event' }}
+          id="value"
+          name="value"
+          value={value ?? ""}
+          onChange={changeHandler}
+        />
+        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search" onClick={handleSubmit}>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <div className="cards">
+        {events?.map(event => {
+          return (
+            <div className="card" key={event.id}>
+              <div>
+                <h2>{event.title}</h2>
+                <h3>{event.organizeruser}</h3>
+                <h3>{event.date}</h3>
+                <h3>{event.starttime} - {event.endtime}</h3>
+                <h3>{event.description}</h3>
+
+                {user ? (
+                  <FindEvent event={event.id}/>
+                ) : (
+                  <></>
+                )
+                }
               </div>
-            );
-          })}
-          </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
