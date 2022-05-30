f1137b2b6be9ec7314e61e219f82a0d0b611f911
RoryHemmings <roryhemmings@ucla.edu>
2022-05-29 21:45:28 -0700

    add error checking

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -81,7 +81,7 @@ const getUser = async (req, res, next) => {
   }
 };
 
-const getUserAddedEvents = async (req, res) => {
+const getUserAddedEvents = async (req, res, next) => {
   try {
     let events = await queryEvents(req.query.userid);
     res.status(200).json({
@@ -92,7 +92,7 @@ const getUserAddedEvents = async (req, res) => {
   }
 }
 
-const getUserCreatedEvents = async (req, res) => {
+const getUserCreatedEvents = async (req, res, next) => {
   try {
     let events = await queryEvents(req.query.userid);
     res.status(200).json({
@@ -103,81 +103,99 @@ const getUserCreatedEvents = async (req, res) => {
   }
 }
 
-const getUserTags = async (request, response) => {
+const getUserTags = async (request, response, next) => {
   let userid = request.query.userid;
+  if (!userid)
+    next({ status: 400, message: 'invalid userid' });
+
   db.query('SELECT likes FROM users WHERE id=$1', [userid], async (error, results) => {
     if (error) {
       console.log(error);
-      return results.status(500).json({ message: 'database error' });
+      return next({ status: 500, message: 'database error' });
     }
     if (results.rowCount < 1) {
-      return response.status(400).json({ message: 'no user with that id exists' });
+      return next({ status: 400, message: 'no user with that id exists' });
     }
     response.status(200).json(results.rows[0]);
   });
 }
 
-const postAddEvent = async (request, response) => {
+const postAddEvent = async (request, response, next) => {
   let eventid = request.body.eventid;
   let userid = request.body.userid;
+  if (!eventid || !userid)
+    next({ status: 400, message: 'invalid eventid or userid' });
+
   db.query('UPDATE users SET addedevents = ARRAY_APPEND(addedevents, $1) WHERE id=$2 AND NOT ($3 = ANY (addedevents))',
     [eventid, userid, eventid], (error, results) => {
       if (error) {
         console.log(error);
-        return response.status(500).send({ message: 'database error' });
+        return next({ status: 500, message: 'database error' });
       }
     });
   db.query('UPDATE events SET num_attendee = num_attendee + 1 WHERE id=$1', [eventid], (error, results) => {
     if (error) {
       console.log(error);
-      return response.status(500).send({ message: 'database error' });
+      return next({ status: 500, message: 'database error' });
     }
-  });
-  response.status(201).send({ add: eventid });
 
+    response.status(201).send({ add: eventid });
+  });
 };
 
-const postRemoveEvent = async (request, response) => {
+const postRemoveEvent = async (request, response, next) => {
   let eventid = request.body.eventid;
   let userid = request.body.userid;
+  if (!eventid || !userid)
+    next({ status: 400, message: 'invalid tagid or userid' });
+
   db.query('UPDATE users SET addedevents = ARRAY_REMOVE(addedevents, $1) WHERE id=$2 AND ($3 = ANY (addedevents))',
     [eventid, userid, eventid], (error, results) => {
       if (error) {
         console.log(error);
-        return response.status(500).send({ message: 'database error' });
+        return next({ status: 500, message: 'database error' });
       }
     });
   db.query('UPDATE events SET num_attendee = num_attendee - 1 WHERE id=$1', [eventid], (error, results) => {
     if (error) {
       console.log(error);
-      return response.status(500).send({ message: 'database error' });
+      return next({ status: 500, message: 'database error' });
     }
+
+    response.status(201).send({ remove: eventid });
   });
-  response.status(201).send({ remove: eventid });
 };
 
-const postAddLike = async (request, response) => {
+const postAddLike = async (request, response, next) => {
   let tagid = request.body.tagid;
   let userid = request.body.userid;
+  if (!tagid || !userid)
+    next({ status: 400, message: 'invalid tagid or userid' });
+
   db.query('UPDATE users SET likes = ARRAY_APPEND(likes, $1) WHERE id=$2 AND NOT ($3 = ANY (likes))',
     [tagid, userid, tagid], (error, results) => {
       if (error) {
-        throw error;
+        return next({ status: 500, message: 'database error' });
       }
+
+      response.status(201).send({ add: tagid });
     });
-  response.status(201).send({ add: tagid });
 };
 
-const postRemoveLike = async (request, response) => {
+const postRemoveLike = async (request, response, next) => {
   let tagid = request.body.tagid;
   let userid = request.body.userid;
+  if (!tagid || !userid)
+    next({ status: 400, message: 'invalid tagid or userid' });
+
   db.query('UPDATE users SET likes = ARRAY_REMOVE(likes, $1) WHERE id=$2 AND ($3 = ANY (likes))',
     [tagid, userid, tagid], (error, results) => {
       if (error) {
-        throw error;
+        next({ status: 200, message: 'database error' });
       }
+
+      response.status(201).send({ response: 'removed' });
     });
-  response.status(201).send({ response: 'removed' });
 };
 
 router.get('/', utils.authenticateToken, getUser);
